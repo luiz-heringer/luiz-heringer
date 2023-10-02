9ddcbb48abfdb67a6330dbc63cafb3cad03eab79
luiz-heringer <58035041+luiz-heringer@users.noreply.github.com>
2023-10-02 10:39:24 -0300

    Create test-polymer2.py

---
+++ test-polymer2.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+config = {
+    'user': 'luiz@loupen.com.br',
+    'phone': '+5544997660652',
+    'password': 123456,
+    'host': 'localhost',
+    'database': 'meu_banco_de_dados',
+}
+
+try:
+    connection = mysql.connector.connect(**config)
+
+    if connection.is_connected():
+        print("Conexão bem-sucedida.")
+
+except mysql.connector.Error as err:
+    print(f"Erro ao conectar: {err}")
+finally:
+    if 'connection' in locals() and connection.is_connected():
+        connection.close()
+        print("Conexão fechada.")
